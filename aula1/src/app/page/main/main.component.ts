096bad32fb367d173d1262516c5bed232e804fac
BarKench <wagnercandido10123@gmail.com>
2019-05-08 11:57:53 -0300

    08052019

--- aula1/src/app/page/main/main.component.ts
+++ aula1/src/app/page/main/main.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Produto } from '../../produto/produto';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -7,8 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  private url: string = environment.url_rest + "produtos";
+  private produtos:Observable<Produto[]>;
+ 
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  public getprodutos() {
+    return this.produtos;
+    return this.http.get<Produto[]>(this.url);
+  }
+
 
   ngOnInit() {
   }
